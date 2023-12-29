cde4f6e4654ff8a5bd24d2e71a6c98bfbfcaa66d
7G-96 <sksms84@naver.com>
2023-12-29 15:24:33 +0900

    상단이동 배지 제작
    최상단으로 이동할 수 있는 배지 제작

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEL.addEventListener('blur', function () {
 
 
 const badgeEL = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
@@ -27,15 +28,30 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display: 'none'
     });
+    // 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
   } else {
     // 배지 보이기
     gsap.to(badgeEL, .6, {
       opacity: 1,
       display: 'block'
     });
+    // 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 
 const fadeELs = document.querySelectorAll('.visual .fade-in');
